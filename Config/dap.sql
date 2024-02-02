2a5b7f0bad52c432bc5f10effe56574fa8ffa11f
budi3312101075 <budiprayoga5103@gmail.com>
2024-02-02 14:53:29 +0700

    up database

--- Config/dap.sql
+++ Config/dap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 01 Feb 2024 pada 07.21
+-- Waktu pembuatan: 02 Feb 2024 pada 07.52
 -- Versi server: 8.0.30
 -- Versi PHP: 8.1.10
 
@@ -24,6 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `keuangan`
+--
+
+CREATE TABLE `keuangan` (
+  `id` int NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `keterangan` longtext NOT NULL,
+  `tanggal` date NOT NULL,
+  `nominal` int NOT NULL,
+  `is_Deleted` tinyint(1) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `kriteria`
 --
 
@@ -38,14 +55,6 @@ CREATE TABLE `kriteria` (
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data untuk tabel `kriteria`
---
-
-INSERT INTO `kriteria` (`id`, `jenis_bantuan`, `nominal`, `keterangan`, `dokumen`, `batas_waktu`, `is_Deleted`, `createdAt`, `updatedAt`) VALUES
-(34, 'bantuan menikah', 1000000, 'untuk anak saya menikah', 'buku menikah', 1, 0, '2024-02-01 03:25:10', '2024-02-01 03:25:10'),
-(35, 'Ut magnam odio commo', 332424, 'Provident quia quia', 'Ut id inventore min', 32, 0, '2024-02-01 04:05:14', '2024-02-01 04:05:14');
 
 -- --------------------------------------------------------
 
@@ -104,6 +113,12 @@ INSERT INTO `users` (`id`, `nama`, `username`, `password`, `role`, `email`, `no_
 --
 
 --
+-- Indeks untuk tabel `keuangan`
+--
+ALTER TABLE `keuangan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `kriteria`
 --
 ALTER TABLE `kriteria`
@@ -128,16 +143,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `keuangan`
+--
+ALTER TABLE `keuangan`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
 -- AUTO_INCREMENT untuk tabel `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengajuan`
 --
 ALTER TABLE `pengajuan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
